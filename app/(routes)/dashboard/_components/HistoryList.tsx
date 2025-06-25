d16ece15b24faa3d2ca1fec358694b5364747b81
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-06-25 23:27:32 +0530

    History list created

--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -1,19 +1,25 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { ClipboardPlus } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 
 const HistoryList = () => {
   const [historyList, setHistoryList] = useState([]);
   return (
-    <div>
+    <div className="mt-10">
       {historyList.length == 0 ? (
-        <div className="flex items-center justify-center gap-4 bg-gray-200 mt-10 shadow rounded-2xl">
+        <div className="flex items-center justify-center gap-4 p-10 bg-gray-100 shadow-md rounded-2xl">
           <div>
             <Image src={"/assistance.png"} alt="" width={300} height={0} />
           </div>
-          <div>
+          <div className="flex flex-col justify-center items-center gap-2">
             <h2 className="font-bold text-xl">No recent Consultations</h2>
             <p>It looks like you havn't consultated to any doctors yet!</p>
+            <Button>
+              <ClipboardPlus />
+                Start a Consultation
+            </Button>
           </div>
         </div>
       ) : (
