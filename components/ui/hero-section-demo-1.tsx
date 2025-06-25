c691339f3c7d09b71d2fce246b40869622959f04
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-06-25 18:55:09 +0530

    Landing page setup

--- components/ui/hero-section-demo-1.tsx
+++ components/ui/hero-section-demo-1.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 
-import { FeaturesBentoGrid } from "@/components/BentoGrid/FeaturesBentoGrid";
 import { motion } from "motion/react";
-import Image from "next/image";
 
-export default function Home () {
+export default function HeroSectionOne() {
   return (
-    <div className="relative mx-2 lg:mx-10 my-10 flex flex-col items-center justify-center">
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <Navbar />
       <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
@@ -20,7 +18,7 @@ export default function Home () {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Get medical answers in seconds, not hours"
+          {"Launch your website in hours, not days"
             .split(" ")
             .map((word, index) => (
               <motion.span
@@ -51,7 +49,9 @@ export default function Home () {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-         CliniVoice is a next-generation AI-powered voice agent built specifically for healthcare. Designed to streamline clinical workflows and enhance patient care, CliniVoice listens, understands, and responds in real-time—making medical communication faster, safer, and smarter.
+          With AI, you can launch your website in hours, not days. Try our best
+          in class, state of the art, cutting edge AI tools to get your website
+          up.
         </motion.p>
         <motion.div
           initial={{
@@ -99,7 +99,6 @@ export default function Home () {
           </div>
         </motion.div>
       </div>
-      <FeaturesBentoGrid/>
     </div>
   );
 }
@@ -108,8 +107,8 @@ const Navbar = () => {
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
-        <Image src="/logo.png" alt="CliniVoice Logo" width={50} height={50}/>
-        <h1 className="text-base font-bold md:text-2xl">CliniVoice</h1>
+        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
+        <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
       <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
