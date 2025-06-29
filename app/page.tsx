2e06e95e7daa0c91eee23103ddbbc82ad0da3dec
Rishi Patodiya <rishipatodiya12@gmail.com>
2025-06-29 12:22:34 +0530

    Voice chat with assistant is done and working with transcipt

--- app/page.tsx
+++ app/page.tsx
@@ -95,13 +95,13 @@ export default function Home() {
           className="relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
         >
           <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
-            <img
+            {/* <img
               src="https://assets.aceternity.com/pro/aceternity-landing.webp"
               alt="Landing page preview"
               className="aspect-[16/9] h-auto w-full object-cover"
               height={1000}
               width={1000}
-            />
+            /> */}
           </div>
         </motion.div>
       </div>
